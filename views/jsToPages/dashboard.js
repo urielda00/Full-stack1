54a17bbe06cb26a0c7e03524842a1f73bfac05f8
urielda00 <urielda00@gmail.com>
2023-02-16 17:43:06 +0200

    basic structure involve

--- views/jsToPages/dashboard.js
+++ views/jsToPages/dashboard.js
@@ -1,4 +1,13 @@
-const user= req.user.name;
+const {pool}= require('/Users/uriel/Desktop/Full-stack/views/db.connect');
+const user= pool.query(`SELECT name FROM users WHERE email=$1`,['uri123@gmail.com'],(err,results)=>{
+    if(err){
+      throw err;
+    }else{
+      console.log(results.rows);
+    }
+    });
+  
+
 let userPname=user.charAt(0).toUpperCase() + user.slice(1);
 document.getElementById("H1").innerHTML = `hello ${user}`;
 
